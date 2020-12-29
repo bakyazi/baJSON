96fb6590c6037aa7bc14c44d425b758fb6c1ce89
bakyazi <berkay@BERKAY-AKYAZI.argela.com.tr>
2020-12-29 12:32:50 +0300

    setuptools init

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="bajson",
+
+    version="0.0.2",
+    author="bakyazi",
+    author_email="berkayakyazi95@gmail.com",
+    description="library for converting to/from json",
+    long_description="library for converting to/from json",
+    url="https://github.com/bakyazi/bajson",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
